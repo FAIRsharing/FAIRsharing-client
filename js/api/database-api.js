4bf2b9806d67b617c9c3abd7e2633369af9fa1a9
zigur <massimorgon@gmail.com>
2016-10-17 18:35:30 +0100

    added api method for TAGS retrieval

--- js/api/database-api.js
+++ js/api/database-api.js
@@ -1,20 +1,36 @@
 import store from '../store';
 import { handleHTTPErrors } from '../utils/helper-funcs';
 import { sendRemoteRequest, getRemoteError } from '../actions/main-actions';
-import { getDatabaseSuccess } from '../actions/database-actions';
-import { API_URL_ROOT, DATABASE_ENDPOINT } from '../utils/api-constants';
+import { getDatabaseSuccess, getTagsSuccess } from '../actions/database-actions';
+import { API_URL_ROOT, DATABASE_ENDPOINT, TAGS_ENDPOINT } from '../utils/api-constants';
 
 export function getDatabase(biodbcoreId) {
     const databaseUrl = `/${API_URL_ROOT}/${DATABASE_ENDPOINT}/${biodbcoreId}`;
     store.dispatch(sendRemoteRequest());
     return fetch(databaseUrl)
         .then(handleHTTPErrors)
-        .then(response => response.json())
+        .then(response => response.json());
+        /*
         .then(json => {
             store.dispatch(getDatabaseSuccess(json));
             return json;
         })
         .catch(err => {
             store.dispatch(getRemoteError(err));
-        });
+        }); */
+}
+
+export function getTags() {
+    const tagsUrl = `/${API_URL_ROOT}/${TAGS_ENDPOINT}/`;
+    store.dispatch(sendRemoteRequest());
+    return fetch(tagsUrl)
+        .then(handleHTTPErrors)
+        .then(response => response.json());
+        /*
+        .then(json => {
+            store.dispatch(getTagsSuccess(json));
+        })
+        .catch(err => {
+            store.dispatch(getRemoteError(err));
+        }); */
 }
