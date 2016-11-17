93d0ca792e4f4f9a4357e6c2b5a80042f60ad806
zigur <massimorgon@gmail.com>
2016-11-17 16:39:01 +0000

    timeline implemented

---
+++ js/components/containers/timeline-container.jsx
@@ -0,0 +1,91 @@
+/**
+* @author massi
+*/
+import '../../../styles/timeline.scss';
+import React from 'react';
+import { isElementInViewport } from '../../utils/helper-funcs';
+import timelineData from '../../../data/timeline.json';
+
+
+class TimelineContainer extends React.Component {
+
+    constructor() {
+        super();
+        this.toggleTimelineElements = this.toggleTimelineElements.bind(this);
+    }
+
+    /**
+     * @method
+     * @name toggleTimelineElements
+     * @description adds the 'in-view' class to elements that are in the viewport
+     */
+    toggleTimelineElements() {
+        for (const ref of Object.keys(this.refs)) {
+            const listElem = this.refs[ref];
+            if (isElementInViewport(listElem)) {
+                listElem.classList.add('in-view');
+            }
+        }
+    }
+
+    componentDidMount() {
+        this.toggleTimelineElements();
+        window.addEventListener('scroll', this.toggleTimelineElements);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.toggleTimelineElements);
+    }
+
+    componentDidUpdate() {
+        this.toggleTimelineElements();
+    }
+
+    render() {
+
+        const timeline = timelineData, eventList = [];
+        let counter = 0;
+
+        for (const event of timeline.data) {
+            const {time, text = '', images = [], links = []} = event, imgs = [], as = [];
+
+            // Add images
+            for (const image of images) {
+                const src = image.src || image;
+                imgs.push(<img key={src} src={src} width={image.width} height={image.height} alt={ image.alt || 'Event Image'} />);
+            }
+
+            // Add links
+            for (const link of links) {
+                as.push(<br />);
+                as.push(<a href={link.href || link} target="_blank" rel="noopener noreferrer">{link.label || link}</a>);
+            }
+
+            eventList.push(<li key={`time-el-${counter}`} ref={counter}>
+                <div>
+                    <time datetime={time.datetime}>{time.display}</time>
+                    {text}
+                    {as}
+                    {imgs}
+                </div>
+            </li>);
+            counter++;
+        }
+
+        return <div className="bs-timeline-cnt">
+            <section className="intro">
+                <div className="bs-timeline-header-cnt">
+                    <h1>BioSharing Timeline &darr;</h1>
+                </div>
+            </section>
+            <section className='bs-timeline'>
+                <ul>
+                    {eventList}
+                </ul>
+            </section>
+        </div>;
+    }
+
+}
+
+export default TimelineContainer;
