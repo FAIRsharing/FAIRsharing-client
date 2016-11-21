67431c1532498ead7e6a51f1e03b54c84f6d4a4a
zigur <massimorgon@gmail.com>
2016-11-21 16:27:15 +0000

    biosharing_web/assets/js/components/containers/graph-container.jsx

--- js/reducers/graph-reducer.js
+++ js/reducers/graph-reducer.js
@@ -8,6 +8,9 @@ values(BIOSHARING_ENTITIES).forEach(entity => visibilityObj[entity.value] = zipO
 const tagSelectorObj = {};
 values(TAG_TYPES).forEach(tagType => tagSelectorObj[tagType.value] = tagType.initialState);
 
+/**
+ * @description the initial state of the graph visualizer  application
+ */
 const initialState = {
     graph: {
         nodes: [],
@@ -56,101 +59,101 @@ const graphReducer = function (state = initialState, action) {
 
     switch (action.type) {
 
-    case types.SEND_REMOTE_REQUEST: {
-        return {
-            ...state,
-            isFetching: true
-        };
-    }
-
-    case types.GET_GRAPH_SUCCESS: {
-        const tagsMap = getUniqueTags(action.graph.nodes);
-        const nextTags = [];
-        for (let [key, value] of tagsMap) {
-            nextTags[key] = {
-                selected: value,
-                unselected: []
+        case types.SEND_REMOTE_REQUEST: {
+            return {
+                ...state,
+                isFetching: true
             };
         }
-        return {
-            ...state,
-            graph: action.graph,
-            isFetching: false,
-            layout: {
-                ...state.layout,
-                tags: {
-                    ...state.layout.tags,
-                    ...nextTags
-                }
-            },
-            reload: true
-        };
-    } // end CASE
 
-    case types.GET_REMOTE_ERROR: {
-        return { ...state, isFetching: false, error: action.error };
-    }
+        case types.GET_GRAPH_SUCCESS: {
+            const tagsMap = getUniqueTags(action.graph.nodes);
+            const nextTags = [];
+            for (let [key, value] of tagsMap) {
+                nextTags[key] = {
+                    selected: value,
+                    unselected: []
+                };
+            }
+            return {
+                ...state,
+                graph: action.graph,
+                isFetching: false,
+                layout: {
+                    ...state.layout,
+                    tags: {
+                        ...state.layout.tags,
+                        ...nextTags
+                    }
+                },
+                reload: true
+            };
+        } // end CASE
 
-    case types.LAYOUT_SELECT_CHANGE:
-        return { ...state, layout: { ...state.layout, ...action.layout }, reload: true };
-
-    case types.VISIBILITY_CHECKBOX_CHANGE: {
-        for (const key of Object.keys(action.visibility)) {
-            action.visibility[key] = Object.assign({}, state.layout.visibility[key], action.visibility[key]);
+        case types.GET_REMOTE_ERROR: {
+            return { ...state, isFetching: false, error: action.error };
         }
-        return {
-            ...state,
-            layout: {
-                ...state.layout,
-                visibility: {
-                    ...state.layout.visibility,
-                    ...action.visibility
-                }
-            },
-            reload: true
-        };
-    }
 
-    case types.TAGS_VISIBILITY_CHECKBOX_CHANGE:
-        return {
-            ...state,
-            layout: {
-                ...state.layout,
-                isTagsPanelVisible: action.isVisible
-            },
-            reload: false
-        };
+        case types.LAYOUT_SELECT_CHANGE:
+            return { ...state, layout: { ...state.layout, ...action.layout }, reload: true };
 
-    case types.DEPTH_CHECKBOX_CHANGE:
-        return {
-            ...state,
-            layout: {
-                ...state.layout,
-                depth: action.depth
-            },
-            reload: true
-        };
+        case types.VISIBILITY_CHECKBOX_CHANGE: {
+            for (const key of Object.keys(action.visibility)) {
+                action.visibility[key] = Object.assign({}, state.layout.visibility[key], action.visibility[key]);
+            }
+            return {
+                ...state,
+                layout: {
+                    ...state.layout,
+                    visibility: {
+                        ...state.layout.visibility,
+                        ...action.visibility
+                    }
+                },
+                reload: true
+            };
+        }
 
-    case types.TAGS_SELECT_CHANGE: {
-        // const nextTags = updateTags(action.tags);
-        return {
-            ...state,
-            layout: {
-                ...state.layout,
-                tags: {
-                    ...state.layout.tags,
-                    ...action.tags
-                }
-            },
-            reload: true
-        };
-    } // end CASE
+        case types.TAGS_VISIBILITY_CHECKBOX_CHANGE:
+            return {
+                ...state,
+                layout: {
+                    ...state.layout,
+                    isTagsPanelVisible: action.isVisible
+                },
+                reload: false
+            };
 
-    case types.OPEN_DETAILS_PANEL:
-        return { ...state, modal: { isOpen: true, node: action.nodeId }, reload: false };
+        case types.DEPTH_CHECKBOX_CHANGE:
+            return {
+                ...state,
+                layout: {
+                    ...state.layout,
+                    depth: action.depth
+                },
+                reload: true
+            };
 
-    case types.CLOSE_DETAILS_PANEL:
-        return { ...state, modal: { isOpen: false, node: null}, reload: false};
+        case types.TAGS_SELECT_CHANGE: {
+            // const nextTags = updateTags(action.tags);
+            return {
+                ...state,
+                layout: {
+                    ...state.layout,
+                    tags: {
+                        ...state.layout.tags,
+                        ...action.tags
+                    }
+                },
+                reload: true
+            };
+        } // end CASE
+
+        case types.OPEN_DETAILS_PANEL:
+            return { ...state, modal: { isOpen: true, node: action.nodeId }, reload: false };
+
+        case types.CLOSE_DETAILS_PANEL:
+            return { ...state, modal: { isOpen: false, node: null}, reload: false};
     }
 
     return state;
