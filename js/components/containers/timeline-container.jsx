b7798a99e08f8f79d2be3d712f2f3851a9a07fdb
zigur <massimorgon@gmail.com>
2016-11-25 19:19:13 +0000

    implemented horizontal timeline

--- js/components/containers/timeline-container.jsx
+++ js/components/containers/timeline-container.jsx
@@ -6,12 +6,70 @@ import React from 'react';
 import { isElementInViewport } from '../../utils/helper-funcs';
 import timelineData from '../../../data/timeline.json';
 
+/**
+ * @class
+ * @name HorizontalTimeline
+ * @extends React.Component
+ */
+export class HorizontalTimeline extends React.Component {
 
-class TimelineContainer extends React.Component {
+    static propTypes = {
+        events: React.PropTypes.array.isRequired
+    }
+
+    constructor() {
+        super();
+        this._buildEventList = this._buildEventList.bind(this);
+    }
+
+    render() {
+        const eventList = this._buildEventList();
+        return <section className='horizontal-timeline'>
+            <div className='bar' />
+            <ul>
+                {eventList}
+            </ul>
+        </section>;
+    }
+
+    /**
+     * @method
+     * @name
+     * @private
+     * @description bulds an array of <li> elements; each element is an entity in the timeline
+     * @param{Array} events
+     * @return Array
+     */
+    _buildEventList() {
+        const { events } = this.props, eventList = [];
+        for (const event of events) {
+            const { time, synopsis, title } = event;
+            if (synopsis) {
+                eventList.push(<li>
+                    <div>
+                        <time dateTime={time.datetime}>{time.display}</time>
+                        {` - ${title}`}
+                    </div>
+                </li>);
+            }
+        }
+        return eventList;
+    }
+
+}
+
+/**
+ * @class
+ * @name VerticalTimeline
+ * @extends React.Component
+ * @prop{Array} events
+ */
+export class VerticalTimeline extends React.Component {
 
     constructor() {
         super();
         this.toggleTimelineElements = this.toggleTimelineElements.bind(this);
+        this._buildEventList = this._buildEventList.bind(this);
     }
 
     /**
@@ -42,24 +100,41 @@ class TimelineContainer extends React.Component {
     }
 
     render() {
+        const eventList = this._buildEventList();
+        return <section className='vertical-timeline'>
+            <ul>
+                {eventList}
+            </ul>
+        </section>;
+    }
 
-        const timeline = timelineData, eventList = [];
+    /**
+     * @method
+     * @name
+     * @private
+     * @description bulds an array of <li> elements; each element is an entity in the timeline
+     * @param{Array} events
+     * @return Array
+     */
+    _buildEventList() {
+        const { events } = this.props, eventList = [];
         let counter = 0;
 
-        for (const event of timeline.data) {
+        for (const event of events) {
             const {time, title, text = '', images = [], links = []} = event, imgs = [], as = [];
 
             // Add images
             for (const image of images) {
                 const src = image.src || image;
-                imgs.push(<br />);
+                imgs.push(<br key={`br-${src}`} />);
                 imgs.push(<img key={src} src={src} width={image.width} height={image.height} alt={ image.alt || 'Event Image'} />);
             }
 
             // Add links
             for (const link of links) {
-                as.push(<br />);
-                as.push(<a href={link.href || link} target="_blank" rel="noopener noreferrer">{link.label || link}</a>);
+                const href = link.href || link;
+                as.push(<br key={`br-${href}`} />);
+                as.push(<a key={href} href={href} target="_blank" rel="noopener noreferrer">{link.label || link}</a>);
             }
 
             eventList.push(<li key={`time-el-${counter}`} ref={counter}>
@@ -73,18 +148,37 @@ class TimelineContainer extends React.Component {
             </li>);
             counter++;
         }
+        return eventList;
+    }
 
+}
+
+/**
+ * @class
+ * @name TimelineContainer
+ * @description container class for the Timelines. It contains a horizontal timeline with summaries
+ *              and a vertical timeline with all the details
+ */
+class TimelineContainer extends React.Component {
+
+    static propTypes = {
+        timelineElements: React.PropTypes.array.isRequired
+    }
+
+    static defaultProps = {
+        timelineElements: timelineData.elements
+    }
+
+    render() {
+        const { timelineElements } = this.props;
         return <div className="bs-timeline-cnt">
+            <HorizontalTimeline events={timelineElements.reverse()} />
             <section className="intro">
                 <div className="bs-timeline-header-cnt">
                     <h1>BioSharing Timeline &darr;</h1>
                 </div>
             </section>
-            <section className='bs-timeline'>
-                <ul>
-                    {eventList}
-                </ul>
-            </section>
+            <VerticalTimeline events={timelineElements}/>
         </div>;
     }
 
