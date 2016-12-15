ed9314fc13a125211c18e5c931a00d2de5e31c33
zigur <massimorgon@gmail.com>
2016-12-15 14:13:49 +0000

    added timeline index file

---
+++ js/timeline-index.jsx
@@ -0,0 +1,7 @@
+// import JavaScript dependencies
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import TimelineContainer from './components/containers/timeline-container';
+
+ReactDOM.render(<TimelineContainer />, document.getElementById('react-root'));
