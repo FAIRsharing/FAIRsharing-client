363598faee6c170a3b375f0cdc7443876c31d458
zigur <massimorgon@gmail.com>
2018-05-14 13:45:18 +0100

    added API key access for tag endpoint

--- js/api/database-api.js
+++ js/api/database-api.js
@@ -2,7 +2,7 @@ import store from '../store';
 import { handleHTTPErrors, serialize } from '../utils/helper-funcs';
 import { sendRemoteRequest } from '../actions/main-actions';
 // import { getDatabaseSuccess, getTagsSuccess } from '../actions/database-actions';
-import { API_URL_ROOT, DATABASE_ENDPOINT, STANDARD_ENDPOINT, TAGS_ENDPOINT } from '../utils/api-constants';
+import { API_URL_ROOT, DATABASE_ENDPOINT, STANDARD_ENDPOINT, TAGS_ENDPOINT, META_TAG_API_KEY } from '../utils/api-constants';
 
 export function getDatabase(biodbcoreId) {
     const databaseUrl = `/${API_URL_ROOT}/${DATABASE_ENDPOINT}/${biodbcoreId}/`;
@@ -10,29 +10,19 @@ export function getDatabase(biodbcoreId) {
     return fetch(databaseUrl)
         .then(handleHTTPErrors)
         .then(response => response.json());
-        /*
-        .then(json => {
-            store.dispatch(getDatabaseSuccess(json));
-            return json;
-        })
-        .catch(err => {
-            store.dispatch(getRemoteError(err));
-        }); */
 }
 
 export function getTags() {
     const tagsUrl = `/${API_URL_ROOT}/${TAGS_ENDPOINT}/`;
+    const apiKey = document.querySelector(`meta[name=${META_TAG_API_KEY}]`).getAttribute('content');
+    const headers = new Headers();
+    headers.append('Api-Key', apiKey);
     store.dispatch(sendRemoteRequest());
-    return fetch(tagsUrl)
+    return fetch(tagsUrl, {
+        headers
+    })
         .then(handleHTTPErrors)
         .then(response => response.json());
-        /*
-        .then(json => {
-            store.dispatch(getTagsSuccess(json));
-        })
-        .catch(err => {
-            store.dispatch(getRemoteError(err));
-        }); */
 }
 
 /**
